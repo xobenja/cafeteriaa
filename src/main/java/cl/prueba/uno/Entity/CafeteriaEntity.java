77e5be124389d7f4cadd338db080b86a07efd434
Jlarx <joh.laram@duocuc.cl>
2025-06-26 00:45:03 -0400

    Entity agregado

--- src/main/java/cl/prueba/uno/Entity/CafeteriaEntity.java
+++ src/main/java/cl/prueba/uno/Entity/CafeteriaEntity.java
@@ -1,5 +1,25 @@
 package cl.prueba.uno.Entity;
 
-public class CafeteriaEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "cafeteria_messiRonaldo")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CafeteriaEntity {
+    @Id
+    @Column(name = "id_cafeteria")
+    private int idCafeteria;
+
+    @Column(name = "nombre_local")
+    private String nombreLocal;
 }
