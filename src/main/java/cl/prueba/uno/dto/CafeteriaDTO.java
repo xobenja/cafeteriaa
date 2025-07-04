2eedc4f716261b684d24410ee3b2ac3dcb1b3e80
xobenja <b.zamorano@duocuc.cl>
2025-07-04 00:54:27 -0400

    arreglé los datos vacios

--- src/main/java/cl/prueba/uno/dto/CafeteriaDTO.java
+++ src/main/java/cl/prueba/uno/dto/CafeteriaDTO.java
@@ -3,6 +3,8 @@ package cl.prueba.uno.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import cl.prueba.empleado.dto.EmpleadoDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +15,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+
 public class CafeteriaDTO {
     private int idCafeteria;
     private String nombreLocal;
